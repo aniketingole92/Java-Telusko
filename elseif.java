f75de313d1e1ca1cb77261abb5d5e2eea22121ef
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-31 13:18:27 +0530

    Create elseif.java

---
+++ elseif.java
@@ -0,0 +1,15 @@
+public class elseif {
+
+    public static void main(String[] args) {
+        int x = 8;
+        int y = 5;
+
+        if(x<y)
+        System.out.println("Super ");
+        else 
+        System.out.println("Invalid super");
+
+
+
+    }
+}
