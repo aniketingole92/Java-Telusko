e0bf43fde477e7afbee088202a597e1604b2b8f5
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-31 13:18:52 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 9;
+
+        if(a<b)
+            System.out.println("Hello Boy");
+
+        else
+        System.out.println("Invalid Syntax");
+
+    }
+}
+
+
+
+
