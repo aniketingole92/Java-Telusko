9f80ed98c1ae69ff51bc54d38dc09c252cabe695
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-31 13:20:01 +0530

    Create or.java

---
+++ or.java
@@ -0,0 +1,9 @@
+public class or {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 9;
+        boolean result = x > y;
+        
+        System.err.println(result);
+    }
+}
