c8564f00281dc695b573c358bcf9f0eb91b0f2c2
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-31 13:19:40 +0530

    Create logicall.java

---
+++ logicall.java
@@ -0,0 +1,15 @@
+ public class logicall {
+
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 5;
+        int a = 5 ;
+        int b = 9;
+
+        boolean result = a > b;
+
+        System.out.println(!result);
+
+    }
+}
+
