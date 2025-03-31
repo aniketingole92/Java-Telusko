f25f1a1b0d3c084ba262b5a4ff489e5cca2519af
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-03-31 13:17:59 +0530

    Create and.java

---
+++ and.java
@@ -0,0 +1,14 @@
+public class and {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int a = 3;
+        int b = 1;
+
+        boolean result = a > b && x < y;
+
+        System.out.println(result);
+
+        
+    }
+}
