3ecd5a24a08d62400bb58b573d72f13f6083ae4b
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-04 10:34:21 +0530

    Create whileedo.java

---
+++ whileedo.java
@@ -0,0 +1,10 @@
+public class whileedo {
+    public static void main(String[] args) {
+        int j = 6;
+        do {
+            System.out.println("Hello " + j);
+            j++;
+            
+        } while (j <=3);
+    }
+}
