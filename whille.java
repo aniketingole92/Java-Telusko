e6d8f4aeb0ddbf6928007a65adad2a6729ad5190
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-04 10:33:54 +0530

    Create whille.java

---
+++ whille.java
@@ -0,0 +1,13 @@
+public class whille {
+    public static void main(String[] args) {
+        int i = 5;
+
+
+        while (i > 2) {
+            System.out.println("Aniket " + i);
+            i++;
+            
+        }
+        System.out.println("Hello Aniket ");
+    }
+}
