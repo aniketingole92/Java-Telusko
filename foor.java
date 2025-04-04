a17c66e06e63327b52b09245a7ec88baebdcb07b
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-04 11:09:57 +0530

    Create foor.java

---
+++ foor.java
@@ -0,0 +1,16 @@
+public class foor {
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        for (;i<=5;)
+        {
+            System.out.println("Day " + i);
+
+            i++;
+
+
+        }
+        
+        }
+}
