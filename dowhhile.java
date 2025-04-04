1e23be0f2a61c7a799e19f773031443ee756171b
Aniket ingole <148335687+aniketingole92@users.noreply.github.com>
2025-04-04 09:14:26 +0530

    Create dowhhile.java

---
+++ dowhhile.java
@@ -0,0 +1,20 @@
+public class dowhilee {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 4) 
+        {
+
+            System.out.println("Hii " + i);
+            
+            i++;
+        }
+        System.out.println("Bye " + i);
+        
+
+
+        
+    }
+}
+
+
